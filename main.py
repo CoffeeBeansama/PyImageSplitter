01c944bfec185f06bd5ce013ceab323b954f0a33
Francis Aigo <103745801+CoffeeBeansama@users.noreply.github.com>
2023-10-09 15:06:49 +0800

    update

---
+++ main.py
@@ -0,0 +1,79 @@
+import cv2
+import tkinter as tk
+from tkinter import filedialog
+from tkfilebrowser import askopenfilename
+
+
+
+font = ("Roboto-Medium.ttf",40)
+font2 = ("Roboto-Medium.ttf",28)
+
+
+window = tk.Tk()
+window.title("Image Splitter")
+
+window.geometry("600x600")
+
+#region browseFile
+def browseFile():
+    file = askopenfilename(parent=window, initialdir=' ', initialfile='png',
+                           filetypes=[("Pictures", "*.png|*.jpg|*.JPG"),
+                                      ("All files", "*")])
+    return str(file)
+
+#endregion
+
+
+rowText = tk.Label(window,text="Rows",font=font2)
+columnText = tk.Label(window,text="Column",font=font2)
+row = tk.Entry(window)
+column = tk.Entry(window)
+
+rowText.place(x=178,y=160)
+columnText.place(x=330,y=160)
+row.place(x=150,y=200)
+column.place(x=320,y=200)
+
+
+#region main function
+def sliceImage():
+    try:
+        path = browseFile()
+        img = cv2.imread(path)
+        img2 = img
+        height, width, channels = img.shape
+        # Number of pieces Horizontally
+        W_SIZE = int(column.get())
+        textToRemove = []
+
+        # Number of pieces Vertically to each Horizontal
+        H_SIZE = int(row.get())
+        imgFile = ""
+        for ih in range(H_SIZE):
+            for iw in range(W_SIZE):
+                x = width / W_SIZE * iw
+                y = height / H_SIZE * ih
+                h = (height / H_SIZE)
+                w = (width / W_SIZE)
+                img = img[int(y):int(y + h), int(x):int(x + w)]
+
+                for i in path[::-1]:
+                    if i != "/":
+                        textToRemove.append(i)
+                    else:
+                        for j in textToRemove[::-1]:
+                            imgFile += j
+                        break
+
+                savePath = path.replace(imgFile,"")
+
+                cv2.imwrite(savePath + str(ih) + str(iw) + ".png", img)
+                img = img2
+    except: pass
+
+#endregion
+
+sliceButton = tk.Button(window,text="Browse Image",command=sliceImage)
+sliceButton.place(x=240,y=250)
+
+window.mainloop()
